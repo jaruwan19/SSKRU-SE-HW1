40a4a2962f5b0b82ae5f5cf47566d48f7e5a530a
jaruwan19 <stu6410014114@sskru.ac.th>
2022-12-10 21:19:45 +0700

    ส่งงานวันที่สอง

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,7 @@
     Reference: https://www.w3schools.com/jsrEF/default.asp
 */
 
-var DEBUG = true;
+var DEBUG = false;
 
 // ตัวอย่างการประกาศตัวแปรในภาษา javascript
 var currentOutput = "";
@@ -49,66 +49,92 @@ function oneClicked()
 function twoClicked()
 {
     dbg("2 is clicked");
+    currentOutput += "2";
+    document.getElementById("display").value = currentOutput;
 }
 
 function threeClicked()
 {
     dbg("3 is clicked");
+    currentOutput += "3";
+    document.getElementById("display").value = currentOutput;
 }
 
 function fourClicked()
 {
     dbg("4 is clicked");
+    currentOutput += "4";
+    document.getElementById("display").value = currentOutput;
 }
 
 function fiveClicked()
 {
     dbg("5 is clicked");
+    currentOutput += "5";
+    document.getElementById("display").value = currentOutput;
 }
 
 function sixClicked()
 {
     dbg("6 is clicked");
+    currentOutput += "6";
+    document.getElementById("display").value = currentOutput;
 }
 
 function sevenClicked()
 {
     dbg("7 is clicked");
+    currentOutput += "7";
+    document.getElementById("display").value = currentOutput;
 }
 
 function eightClicked()
 {
     dbg("8 is clicked");
+    currentOutput += "8";
+    document.getElementById("display").value = currentOutput;
 }
 
 function nineClicked()
 {
     dbg("9 is clicked");
+    currentOutput += "9";
+    document.getElementById("display").value = currentOutput;
 }
 
 function zeroClicked()
 {
     dbg("0 is clicked");
+    currentOutput += "0";
+    document.getElementById("display").value = currentOutput;
 }
 
 function dotClicked()
 {
     dbg(". is clicked");
+    currentOutput += ".";
+    document.getElementById("display").value = currentOutput;
 }
 
 function plusClicked()
 {
     dbg("+ is clicked");
+    currentOutput += "+";
+    document.getElementById("display").value = currentOutput;
 }
 
 function minusClicked()
 {
     dbg("- is clicked");
+    currentOutput += "-";
+    document.getElementById("display").value = currentOutput;
 }
 
 function multiplyClicked()
 {
     dbg("x is clicked");
+    currentOutput += "*";
+    document.getElementById("display").value = currentOutput;
 
     /* คำแนะนำ
         ตัวคูณในภาษาจาวาสคริปคือ *
@@ -118,6 +144,8 @@ function multiplyClicked()
 function divideClicked()
 {
     dbg("÷ is clicked");
+    currentOutput += "/";
+    document.getElementById("display").value = currentOutput;
 
     /* คำแนะนำ
         ตัวหารในภาษาจาวาสคริปคือ /
@@ -127,6 +155,9 @@ function divideClicked()
 function equalClicked()
 {
     dbg("= is clicked");
+    currentOutput = eval(currentOutput);
+    document.getElementById("display").value = currentOutput;
+
 
     /* คำแนะนำ 
         ให้ใช้ฟังก์ชั่น eval ในการคำนวนค่าบวกลบคูณหาร
@@ -136,12 +167,14 @@ function equalClicked()
     */
 
     // ตัวอย่างการแสดงค่าออกทาง console เพื่อดูค่าของตัวแปร currentOutput
-    dbg("ค่าของ currentOutput คือ ", currentOutput);
+    // dbg("ค่าของ currentOutput คือ ", currentOutput);
 }
 
 function delClicked()
 {
     dbg("del is clicked");
+    currentOutput = currentOutput.slice(0, -1);
+    document.getElementById("display").value = currentOutput;
 
     /* คำแนะนำ
         ให้ใช้ฟังก์ชั่น substring() หรือ slice() เพื่อลบตัวอักษรท้ายสุด 
@@ -160,6 +193,8 @@ function delClicked()
 function clearClicked()
 {
     dbg("C is clicked");
+    currentOutput = "";
+    document.getElementById("display").value = currentOutput;
 
     /* คำแนะนำ
         ให้ล้างค่า currentOutput และอัพเดท HTML text input id="display" โดยการใช้ document.getElementById()
@@ -174,9 +209,13 @@ function clearClicked()
 function poClicked()
 {
     dbg("( is clicked");
+    currentOutput += "(";
+    document.getElementById("display").value = currentOutput;
 }
 
 function pcClicked()
 {
     dbg(") is clicked");
+    currentOutput += ")";
+    document.getElementById("display").value = currentOutput;
 }
